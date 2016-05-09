c081bd65cba81249be675e30dd16f57677443c80
Kristian Maier <kristian.maier@gmx.de>
2016-05-10 00:55:08 +0200

    simplified the carrier mapping code, no change in function

--- transmitter_model/carrier_mapping.m
+++ transmitter_model/carrier_mapping.m
@@ -1,10 +1,12 @@
 function [ A ] = carrier_mapping(symbols, fft_len, pilot_method )
-%CARRIER_MAPPING Summary of this function goes here
+%CARRIER_MAPPING Maps the symbols into the time-freq grid A(k,l) and
+%inserts the pilot symbols.
 %   pilot_method: 'A' or 'B'
 
 assert(pilot_method == 'A' | pilot_method == 'B');
 assert(fft_len == 32 | fft_len == 64 | fft_len == 128);
 
+%for method A and B
 unused_carriers_32 = [-16,-15,-14,0,14,15];
 unused_carriers_64 = [-32,-31,-30,-29,0,29,30,31];
 unused_carriers_128 = [-64,-63,-62,-61,-60,0,60,61,62,63];
@@ -28,76 +30,58 @@ elseif (fft_len == 128)
 end
 
 
-ofdm_symbol_generated = 0;
-tx_sym_processed = 0;
-tx_sym_cnt = length(symbols);
-
-%A = zeros(fft_len,1);
-
 if(pilot_method == 'A')
-    finish = 0;
+    tx_sym_per_ofdm_sym = fft_len-length(unused_carriers);
+    used_carriers = -fft_len/2:fft_len/2-1;
+    used_carriers(unused_carriers+fft_len/2+1) = [];
     
-    while ~finish
-        %init time/freq grid
-        A(:,ofdm_symbol_generated+1) = zeros(fft_len,1);
-        
-        %map pilot_carriers
-        if( mod(ofdm_symbol_generated,2) == 0)
-            k=0;
-            for i=-fft_len/2:fft_len/2-1
-                k = k+1;
-                if(~any(i==unused_carriers))    %dont use unused carriers
-                    A(k,ofdm_symbol_generated+1) = 1;
-                end
-            end
-            ofdm_symbol_generated=ofdm_symbol_generated+1;
-        else
-            %map symbols
-            k=0;
-            for i=-fft_len/2:fft_len/2-1
-                k = k+1;
-                if(~any(i==unused_carriers))    %dont use unused carriers
-                    if(tx_sym_processed == tx_sym_cnt)
-                        finish = 1;
-                        break;
-                    end
-                    A(k,ofdm_symbol_generated+1) = symbols(tx_sym_processed+1);
-                    tx_sym_processed = tx_sym_processed+1;
-                end
-            end
-            ofdm_symbol_generated=ofdm_symbol_generated+1;
-        end
+    %check for correct symbol length to fill whole ofdm symbols
+    m=mod(length(symbols),tx_sym_per_ofdm_sym);
+    if(m ~= 0)
+        %padding to fill a complete ofdm symbol
+        z = tx_sym_per_ofdm_sym-m;
+        symbols = [symbols zeros(1,z)];
+        warning('carrier mapping method A: symbols given doesnt fill whole ofdm symbols, now using padding with zeroes')
     end
+    
+    symbols = reshape(symbols,tx_sym_per_ofdm_sym,[]);
+    
+    %insert pilots on even subcarriers
+    symbols = reshape([ones(size(symbols)); symbols], tx_sym_per_ofdm_sym, []);
+    %init time freq grid
+    A = zeros(fft_len, size(symbols,2));
+    %insert symbols on grid
+    A(used_carriers+fft_len/2+1,:) = symbols;
+    
+    
 end
 
 
 if(pilot_method == 'B')
-    finish = 0;
+    tx_sym_per_ofdm_sym = fft_len-length(unused_carriers)-length(ofdm_pilot_carriers);
+    used_carriers = -fft_len/2:fft_len/2-1;
+    used_carriers([unused_carriers ofdm_pilot_carriers]+fft_len/2+1) = [];
     
-    while ~finish
-        %init time/freq grid
-        A(:,ofdm_symbol_generated+1) = zeros(fft_len,1);
-        
-        k=0;
-        for i=-fft_len/2:fft_len/2-1
-            k = k+1;
-            if(~any(i==unused_carriers))    %dont use unused carriers
-                
-                %map pilots
-                if(any(i==ofdm_pilot_carriers))
-                    A(k,ofdm_symbol_generated+1) = 1;
-                elseif(tx_sym_processed == tx_sym_cnt)
-                    finish = 1;                   
-                else
-                    A(k,ofdm_symbol_generated+1) = symbols(tx_sym_processed+1);
-                    tx_sym_processed = tx_sym_processed+1;
-                end               
-            end            
-            
-        end
-        ofdm_symbol_generated=ofdm_symbol_generated+1;
-        
+    %check for correct symbol length to fill whole ofdm symbols
+    m=mod(length(symbols),tx_sym_per_ofdm_sym);
+    if(m ~= 0)
+        %padding to fill a complete ofdm symbol
+        z = tx_sym_per_ofdm_sym-m;
+        symbols = [symbols zeros(1,z)];
+        warning('carrier mapping method B: symbols given doesnt fill whole ofdm symbols, now using padding with zeroes')
     end
-    
+    symbols = reshape(symbols,tx_sym_per_ofdm_sym,[]);
+    %init time freq grid
+    A = zeros(fft_len, size(symbols,2));    
+    %insert symbols on grid
+    A(used_carriers+fft_len/2+1,:) = symbols;  
+    %insert pilots on grid
+    A(ofdm_pilot_carriers+fft_len/2+1,:) = 1;
 end
+
+
+
+
+
+
 
