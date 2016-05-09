aba9d9c20c3728eecfcb8b21c9f23baf2684ea1c
Christoph Rist <rist.christoph@gmail.com>
2016-05-09 19:18:37 +0200

    Fix test case for cyclic prefix adding

--- tests/TestCyclicPrefixAdding.m
+++ tests/TestCyclicPrefixAdding.m
@@ -2,15 +2,16 @@ classdef TestCyclicPrefixAdding < matlab.unittest.TestCase
     
     properties (TestParameter)
         inputs = {[ 1; 2; 3; 4], [1 5; 2 6; 3 7; 4 8]};
-        outputs = {[ 4; 1; 2; 3; 4], [ 4 8; 1 5; 2 6; 3 7; 4 8]};
+        outputs = {[ 4 1 2 3 4], [ 4 1 2 3 4 8 5 6 7 8]};
     end
     
     
     methods (Test, ParameterCombination='sequential')
         function testSimpleOutput(testCase, inputs, outputs)
+            addpath ../transmitter_model
             import matlab.unittest.qualifications.Verifiable
             
-            verifyEqual(testCase, inputs, outputs);            
+            verifyEqual(testCase, cyclic_prefix_adding(inputs), outputs);            
         end
     end
     
