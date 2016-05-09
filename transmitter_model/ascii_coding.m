5d5f7cf1e84ce157b6bc8fba32a2bd64d6dc52ab
Kristian Maier <kristian.maier@gmx.de>
2016-05-09 23:20:17 +0200

    now correct ascii coding in tx-model

--- transmitter_model/ascii_coding.m
+++ transmitter_model/ascii_coding.m
@@ -2,7 +2,7 @@ function [ b_n ] = ascii_coding( c_m )
 %ASCII_CODING Summary of this function goes here
 %   returns double vector of zeros and ones ascii coded
 
-b_n = reshape(dec2bin(c_m, 8), 1, []);
+b_n = reshape(dec2bin(c_m, 8)', 1, []);
 b_n = b_n-'0';
 
 end
