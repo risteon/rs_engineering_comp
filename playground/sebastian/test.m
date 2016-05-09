8da61349b5306e98afadda2a6bdc63ab7eda5f71
Sebastian Müller <senpo@posteo.de>
2016-05-09 22:19:57 +0200

    Implement test demodulator

--- playground/sebastian/test.m
+++ playground/sebastian/test.m
@@ -1,8 +1,34 @@
-n_symbols = length(Signal)/80
+prefix_len = 16;
+symbol_len = 64;
+unused_carr = [1, 2, 3, 4, 32, 62, 63, 64];
+mod_scheme = 4;
+pilot = 'A';
 
-sig = []
+
+%% nothing to change here
+window_len = prefix_len + symbol_len;
+n_symbols = length(Signal)/window_len;
+
+sig = [];
+% cut out cp
 for i = 1:n_symbols
-    sig = [sig Signal((i-1)*80+16:(i-1)*80+80)];
+    sig = [sig Signal((i-1)*window_len+prefix_len+1:...
+            i*window_len)];
 end
-fft_sig = fft(sig);
-fft_sig = reshape(fft_sig, n_symbols, [])
+
+% create matrix with OFDM symbol in every column
+sig = reshape(sig, symbol_len, []);
+
+if pilot == 'A'
+    % delete pilot symbols
+    sig(:,1:2:size(sig,2)) = [];
+end
+
+fft_sig = fftshift(fft(sig));
+% delete unused carriers
+fft_sig(unused_carr,:) = [];
+
+syms = pskdemod(fft_sig, mod_scheme, pi/mod_scheme);
+syms = dec2bin(syms, log2(mod_scheme))';
+syms = reshape(syms, 8, []).';
+text = char(bin2dec(syms))'
