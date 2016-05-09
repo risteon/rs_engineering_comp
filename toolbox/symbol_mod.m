020da5d534f7a9fb7938ec0dec104d8cbd663a7c
Sebastian Müller <senpo@posteo.de>
2016-05-09 18:56:13 +0200

    Added semikolons (lol)
    and switched parameter order
    of symbol modulation block

--- toolbox/symbol_mod.m
+++ toolbox/symbol_mod.m
@@ -1,4 +1,4 @@
-function [ s ] = symbol_mod( const_points, b )
+function [ s ] = symbol_mod( b, const_points )
 %symbol_mod does PSK modulation for binary input vector
 %   b: binary row vector
 %   const_points: number of psk points (2, 4 or 8)
@@ -6,25 +6,25 @@ function [ s ] = symbol_mod( const_points, b )
 
 if const_points == 2
     % nothing to do here
-    s = pskmod(b, 2)   
+    s = pskmod(b, 2);
 elseif const_points == 4
     % group by 2 bits
-    b = reshape(b, 2, [])
-    dec = []
+    b = reshape(b, 2, []);
+    dec = [];
     % convert binary columns to int
     for i = 1:size(b,2)
-       dec = [dec bi2de(b(:,i)')]
+       dec = [dec bi2de(b(:,i)')];
     end
-    s = pskmod(dec, 4, pi/4)
+    s = pskmod(dec, 4, pi/4);
 elseif const_points == 8
     % group by 4 bits
-    b = reshape(b, 3, [])
-    dec = []
+    b = reshape(b, 3, []);
+    dec = [];
     % convert binary columns to int
     for i = 1:size(b,2)
-        dec = [dec bi2de(b(:,i)')]
+        dec = [dec bi2de(b(:,i)')];
     end
-    s = pskmod(dec, 8)
+    s = pskmod(dec, 8);
 end
 
 end
