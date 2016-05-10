be893c50e9f15562f5a867b9e1473d55a69eed1f
Kristian Maier <kristian.maier@gmx.de>
2016-05-10 18:33:46 +0200

    reapply changes after cleaning up

--- toolbox/channel_estimation_methA_simple.m
+++ toolbox/channel_estimation_methA_simple.m
@@ -5,21 +5,7 @@ function [ Hvec ] = channel_estimation_methA_simple( A )
 %   A: rx time freq grid
 %   Hvec: estimated channel 
 
-fft_len = size(A,1);
-ofdm_symbols = size(A,2);
-assert(fft_len==32 | fft_len ==64| fft_len== 128, 'wrong fft_len');
-pilot = pilot_gen_freq('A',fft_len);
-
-
-%delete unused carriers
-for i=1:ofdm_symbols
-   A(:,i)=A(:,i).*pilot; 
-end
-% average of the pilots
 Hvec = sum(A(:,1:2:end),2) / size(A(:,1:2:end),2);
-
-
-
 
 
 end
