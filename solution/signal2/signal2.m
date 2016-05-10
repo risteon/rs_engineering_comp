656e8228a2040bd91ab90fecee05954cb74dc3fa
Sebastian Müller <senpo@posteo.de>
2016-05-10 20:12:02 +0200

    Added some SNR and channel estimation for signal 2

--- solution/signal2/signal2.m
+++ solution/signal2/signal2.m
@@ -19,10 +19,21 @@ H=fft_sig(:,1:2:end);
 %simple channel correction
 fft_sig(:,2:2:end) = fft_sig(:,2:2:end)./conj(H);
 
-fft_sig = remove_pilot(fft_sig, 'A', symbol_len);
+%fft_sig = remove_pilot(fft_sig, 'A', symbol_len);
 
 plot(reshape(fft_sig,[],1),'*');
 
 % psk demodulation
 syms = pskdemod(fft_sig, mod_scheme,0*pi/4);
 ascii_decoding(syms, mod_scheme)
+
+% calculate channel
+H_abs = abs(H);
+H_abs = mean(H_abs.')'
+
+% calculate SNR
+pilots = mean(fft_sig(:,1:2:end)')';
+p_var = var(pilots);
+signals = mean(fft_sig(:,2:2:end)')';
+s_var = var(signals);
+SIR = (s_var-p_var)/p_var
