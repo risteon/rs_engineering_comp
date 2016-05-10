e94335de0f7d3beb668af7fa844e448713934ba2
Christoph Rist <rist.christoph@gmail.com>
2016-05-10 17:25:40 +0200

    Estimate delay for pilots method A

---
+++ transmitter_model/tests/TestTSyncPilotA.m
@@ -0,0 +1,21 @@
+classdef TestTSyncPilotA < matlab.unittest.TestCase
+    
+    properties (TestParameter)
+        inputs = {0, 1, 2, 3, 20, 42};
+    end
+    
+    
+    methods (Test, ParameterCombination='sequential')
+        function testInputs(testCase, inputs)
+            addpath ../toolbox
+
+            % generate test signal
+            tx = signal_generator('long enough ascii text to generate a signal', 4, 64, 'A');
+            
+            rx = channel_model(tx, inputs);
+            
+            testCase.verifyEqual(t_sync_pilot_A(rx, 64), inputs);
+        end
+    end
+    
+end
---
+++ transmitter_model/toolbox/t_sync_pilot_A.m
@@ -0,0 +1,32 @@
+function [ offset ] = t_sync_pilot_A( tx_in, fft_len )
+%T_SYNC_PILOT_A delay estimation using time domain correlation with pilot
+%signal
+
+assert(exist('tx_in', 'var') ~= 0, 'tx in not set');
+
+pilot_symbol = pilot_gen_freq('A', fft_len);
+pilot_symbol_time_domain = cyclic_prefix_adding(tm_ifft(pilot_symbol));
+
+pilot_length = size(pilot_symbol_time_domain, 2);
+considerable_length = max(pilot_length, size(tx_in, 2));
+
+[time_corr, lags] = xcorr(tx_in, pilot_symbol_time_domain);
+time_corr = time_corr(considerable_length:end);
+
+% superposition 2-symbol sections of correlation for pilots in method A
+superpositioned = zeros(1, 2*pilot_length);
+startpoint = 1;
+endpoint = 2*pilot_length;
+
+while endpoint <= considerable_length
+    superpositioned = superpositioned + time_corr(startpoint:endpoint);
+    startpoint = endpoint + 1;
+    endpoint = endpoint + 2*pilot_length;
+end
+
+% find maximum position and calculate shift
+[~, idx] = max(superpositioned);
+offset = lags(considerable_length -1 + idx);
+
+end
+
