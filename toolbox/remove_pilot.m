30a7081daaa7b8509c0f4a209607b76548811db4
Sebastian Müller <senpo@posteo.de>
2016-05-11 17:04:39 +0200

    Fix indizes for pilot method B. closes #1

--- toolbox/remove_pilot.m
+++ toolbox/remove_pilot.m
@@ -8,15 +8,15 @@ if (~exist('fft_len', 'var'))
     fft_len = true;
 end
 if method == 'A'
-    % delete pilot symbols in every odd column
+    % delete pilot symbols after removed unused carriers
     in_sig(:,1:2:size(in_sig,2)) = [];
 elseif method == 'B'
     if fft_len == 32
-        pilot_indixes = [4 13 21 30];
+        pilot_indixes = [1 10 17 26];
     elseif fft_len == 64
-        pilot_indixes = [5 13 21 29 37 45 53 61]
+        pilot_indixes = [1 9 17 25 32 40 48 56]
     elseif fft_len == 128
-        pilot_indixes = [6 13 21 29 37 45 53 61 69 77 85 93 101 109 117 124]
+        pilot_indixes = [1 8 16 24 32 40 48 56 63 71 79 87 95 103 111 118]
     end
     in_sig(pilot_indixes,:) = [];
 end
