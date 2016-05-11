50ba5f7d0e9ff69ff874fc5354fd87133a7ca5bf
Kristian Maier <kristian.maier@gmx.de>
2016-05-12 01:04:38 +0200

    signal 3 decoded? text message is encrypted

--- solution/signal3/signal3.m
+++ solution/signal3/signal3.m
@@ -11,10 +11,10 @@ symbol_len = 4*prefix_len;
 fft_sig = shape_ofdm(Signal, symbol_len, prefix_len);
 
 %channel estimation
-H = channel_estimation_methB_linear1dInterpolation(fft_sig, symbol_len);
+H = channel_estimation_methB_2DInterpolation(fft_sig, symbol_len,'linear');
 
 %channel correction with H
-fft_sig = fft_sig./conj(H);
+fft_sig = fft_sig.*conj(H)./((abs(H)).^2);
 
 %delete unused carriers and pilots
 fft_sig([5 13 21 29 37 45 53 61 1 2 3 4 33 62 63 64],:)=[];
@@ -22,5 +22,5 @@ fft_sig([5 13 21 29 37 45 53 61 1 2 3 4 33 62 63 64],:)=[];
 plot(reshape(fft_sig,[],1),'*');
 
 % psk demodulation
-syms = pskdemod(fft_sig, mod_scheme,0*pi/4);
+syms = pskdemod(fft_sig, mod_scheme,1*pi/4);
 ascii_decoding(syms, mod_scheme)
