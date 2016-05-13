a6fecc8482d671c0781808cbc14d63ffeec67ca0
Sebastian Müller <senpo@posteo.de>
2016-05-13 14:28:53 +0200

    Add solution note for signal 3

--- solution/signal3/signal3.m
+++ solution/signal3/signal3.m
@@ -19,8 +19,11 @@ fft_sig = fft_sig.*conj(H)./((abs(H)).^2);
 %delete unused carriers and pilots
 fft_sig([5 13 21 29 37 45 53 61 1 2 3 4 33 62 63 64],:)=[];
 
-plot(reshape(fft_sig,[],1),'*');
+%plot(reshape(fft_sig,[],1),'*');
 
 % psk demodulation
 syms = pskdemod(fft_sig, mod_scheme,1*pi/4);
 ascii_decoding(syms, mod_scheme)
+
+% result is kenny speech: http://www.namesuppressed.com/kenny/
+% results to "Von A nach B schneller denn je. LX138"
