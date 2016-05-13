8a0f9cdd55d56d5ab992376e3e6602465bbb548a
Kristian Maier <kristian.maier@gmx.de>
2016-05-13 14:38:20 +0200

    little change in channel est

--- toolbox/channel_estimation_methA_simple.m
+++ toolbox/channel_estimation_methA_simple.m
@@ -1,12 +1,13 @@
-function [ Hvec ] = channel_estimation_methA_simple( A )
+function [ H ] = channel_estimation_methA_simple( A )
 %CHANNEL_ESTIMATION_METHA_SIMPLE estimation of the channel
 %   using a simple average over all pilots 
 %   (assumption: constant channel!)
-%   A: rx time freq grid
-%   Hvec: estimated channel 
+%   in A: rx time freq grid
+%   out H: estimated channel 
 
-Hvec = sum(A(:,1:2:end),2) / size(A(:,1:2:end),2);
+H= sum(A(:,1:2:end),2) / size(A(:,1:2:end),2);
 
+H = repmat(H,1,size(A,2));
 
 end
 
