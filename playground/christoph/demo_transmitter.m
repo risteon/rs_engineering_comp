93943be6b15ba67d4b9f4c4ce7aeaf4fe376ad19
Christoph Rist <rist.christoph@gmail.com>
2016-05-23 20:06:09 +0200

    Update demo transmitter to test signal generator

--- playground/christoph/demo_transmitter.m
+++ playground/christoph/demo_transmitter.m
@@ -1,40 +1,63 @@
 %% Parameters
 addpath('../../toolbox');
-data = 'test';
+data = 'test test test test test test test';
 %   const_points: number of psk points (2, 4 or 8)
-psk_points = 4;
+psk_points = 8;
 %   pilot_method: 'A' or 'B'
 pilot_method = 'A';
 %   fft_len 32, 64, 128
-fft_len = 32;
+fft_len = 64;
 
+%% TX/RX signal
 
-%% ASCII CODING
-disp(['Input c(m) for ascii coding: ', data])
+tx = signal_generator(data, psk_points, fft_len, pilot_method);
+
+% no channel
+rx = tx;
+
+%% Demodulation
+
+%do ofdm operations
+fft_sig = shape_ofdm(rx, fft_len, fft_len/4);
+
+%compensate resulting phase drift from offset
+%drehvector = 2j*pi*offset/fft_len*(0:fft_len-1).';
+%fft_sig = fft_sig .* repmat(exp(drehvector),1,size(fft_sig,2));
+
+%channel estimation
+%H = channel_estimation_methB_2DInterpolation(fft_sig, symbol_len,'linear');
+%H = 1;
+
+%channel correction with H
+%fft_sig = fft_sig./H;
+
+%delete unused carriers and pilots
+fft_sig = remove_unused(fft_sig, fft_len);
+fft_sig = remove_pilot(fft_sig, pilot_method, fft_len);
+
+% psk demodulation
+syms = psk2bitstring(fft_sig, psk_points);
+
+received_text = ascii_decoding(syms);
 
 b_n = ascii_coding(data);
 
 
 %% Symbol Modulation
-disp(['Input b_n0 for 4-PSK: ', num2str(b_n)])
 
 s_n1 = symbol_mod(b_n, psk_points);
 
 %% Carrier Mapping
-disp(['Input s_n1 for Carrier mapping: ', num2str(s_n1)])
 
 A = carrier_mapping(s_n1, fft_len, pilot_method);
 
 %% IFFT
-disp('Input A for IFFT: ');
-disp(A);
 
 a = tm_ifft(A);
 
 %% Cyclic Prefix
-disp('Input a for cyclic prefix adding: ');
-disp(a);
 
-tx = cyclic_prefix_adding(a);
+tx_out = cyclic_prefix_adding(a);
 
-disp(['Output tx: ', num2str(tx)]);
+syms = psk2bitstring(s_n1, psk_points);
+text = ascii_decoding(syms)
